a99c5bbf1a9481710376abe94dd6bf38c31a110b
shodiq <shodiq.fathoni29@gmail.com>
2024-05-17 19:44:12 +0700

    first commit

---
+++ data_preprocessing.py
@@ -0,0 +1,105 @@
+import joblib
+import numpy as np
+import pandas as pd
+
+encoder_Application_mode = joblib.load("model/encoder_Application_mode.joblib")
+encoder_Course = joblib.load("model/encoder_Course.joblib")
+encoder_Debtor = joblib.load("model/encoder_Debtor.joblib")
+encoder_Displaced = joblib.load("model/encoder_Displaced.joblib")
+encoder_Educational_special_needs = joblib.load("model/encoder_Educational_special_needs.joblib")
+encoder_Gender = joblib.load("model/encoder_Gender.joblib")
+encoder_International = joblib.load("model/encoder_International.joblib")
+encoder_Marital_status = joblib.load("model/encoder_Marital_status.joblib")
+encoder_Previous_qualification = joblib.load("model/encoder_Previous_qualification.joblib")
+encoder_Scholarship_holder = joblib.load("model/encoder_Scholarship_holder.joblib")
+encoder_Tuition_fees_up_to_date = joblib.load("model/encoder_Tuition_fees_up_to_date.joblib")
+encoder_attendance = joblib.load("model/encoder_attendance.joblib")
+pca_1 = joblib.load("model/pca_1.joblib")
+pca_2 = joblib.load("model/pca_2.joblib")
+scaler_Admission_grade = joblib.load("model/scaler_Admission_grade.joblib")
+scaler_Age_at_enrollment = joblib.load("model/scaler_Age_at_enrollment.joblib")
+scaler_Curricular_units_1st_sem_approved = joblib.load("model/scaler_Curricular_units_1st_sem_approved.joblib")
+scaler_Curricular_units_1st_sem_credited = joblib.load("model/scaler_Curricular_units_1st_sem_credited.joblib")
+scaler_Curricular_units_1st_sem_enrolled = joblib.load("model/scaler_Curricular_units_1st_sem_enrolled.joblib")
+scaler_Curricular_units_1st_sem_evaluations = joblib.load("model/scaler_Curricular_units_1st_sem_evaluations.joblib")
+scaler_Curricular_units_1st_sem_grade =joblib.load("model/scaler_Curricular_units_1st_sem_grade.joblib")
+scaler_Curricular_units_1st_sem_without_evaluations = joblib.load("model/scaler_Curricular_units_1st_sem_without_evaluations.joblib")
+scaler_Curricular_units_2nd_sem_approved = joblib.load("model/scaler_Curricular_units_2nd_sem_approved.joblib")
+scaler_Curricular_units_2nd_sem_credited = joblib.load("model/scaler_Curricular_units_2nd_sem_credited.joblib")
+scaler_Curricular_units_2nd_sem_enrolled = joblib.load("model/scaler_Curricular_units_2nd_sem_enrolled.joblib")
+scaler_Curricular_units_2nd_sem_evaluations = joblib.load("model/scaler_Curricular_units_2nd_sem_evaluations.joblib")
+scaler_Curricular_units_2nd_sem_grade = joblib.load("model/scaler_Curricular_units_2nd_sem_grade.joblib")
+scaler_Curricular_units_2nd_sem_without_evaluations = joblib.load("model/scaler_Curricular_units_2nd_sem_without_evaluations.joblib")
+scaler_Previous_qualification_grade = joblib.load("model/scaler_Previous_qualification_grade.joblib")
+
+pca_numerical_columns_1 = [
+    'Age_at_enrollment',
+    'Curricular_units_1st_sem_credited',
+    'Curricular_units_1st_sem_enrolled',
+    'Curricular_units_1st_sem_evaluations',
+    'Curricular_units_1st_sem_approved',
+    'Curricular_units_1st_sem_grade',
+    'Curricular_units_1st_sem_without_evaluations',
+    'Curricular_units_2nd_sem_credited',
+    'Curricular_units_2nd_sem_enrolled',
+    'Curricular_units_2nd_sem_evaluations',
+    'Curricular_units_2nd_sem_approved',
+    'Curricular_units_2nd_sem_grade',
+    'Curricular_units_2nd_sem_without_evaluations'
+]
+
+pca_numerical_columns_2 = [
+    'Previous_qualification_grade',
+    'Admission_grade'
+]
+
+def data_preprocessing(data):
+    """Preprocessing data
+
+    Args:
+        data (Pandas DataFrame): Dataframe that contain all the data to make prediction
+
+    return:
+        Pandas DataFrame: Dataframe that contain all the preprocessed data
+    """
+    data = data.copy()
+    df = pd.DataFrame()
+
+
+    df["Marital_status"] = encoder_Marital_status.transform(data["Marital_status"])
+    df["Application_mode"] = encoder_Application_mode.transform(data["Application_mode"])
+    df["Course"] = encoder_Course.transform(data["Course"])
+    df["attendance"] = encoder_attendance.transform(data["attendance"])
+    df["Previous_qualification"] = encoder_Previous_qualification.transform(data["Previous_qualification"])
+    df["Displaced"] = encoder_Displaced.transform(data["Displaced"])
+    df["Educational_special_needs"] = encoder_Educational_special_needs.transform(data["Educational_special_needs"])
+    df["Debtor"] = encoder_Debtor.transform(data["Debtor"])
+    df["Tuition_fees_up_to_date"] = encoder_Tuition_fees_up_to_date.transform(data["Tuition_fees_up_to_date"])
+    df["Gender"] = encoder_Gender.transform(data["Gender"])
+    df["Scholarship_holder"] = encoder_Scholarship_holder.transform(data["Scholarship_holder"])
+    df["International"] = encoder_International.transform(data["International"])
+
+    # PCA 1
+    data["Age_at_enrollment"] = scaler_Age_at_enrollment.transform(np.asarray(data["Age_at_enrollment"]).reshape(-1,1))[0]
+    data["Curricular_units_1st_sem_credited"] = scaler_Curricular_units_1st_sem_credited.transform(np.asarray(data["Curricular_units_1st_sem_credited"]).reshape(-1,1))[0]
+    data["Curricular_units_1st_sem_enrolled"] = scaler_Curricular_units_1st_sem_enrolled.transform(np.asarray(data["Curricular_units_1st_sem_enrolled"]).reshape(-1,1))[0]
+    data["Curricular_units_1st_sem_evaluations"] = scaler_Curricular_units_1st_sem_evaluations.transform(np.asarray(data["Curricular_units_1st_sem_evaluations"]).reshape(-1,1))[0]
+    data["Curricular_units_1st_sem_approved"] = scaler_Curricular_units_1st_sem_approved.transform(np.asarray(data["Curricular_units_1st_sem_approved"]).reshape(-1,1))[0]
+    data["Curricular_units_1st_sem_grade"] = scaler_Curricular_units_1st_sem_grade.transform(np.asarray(data["Curricular_units_1st_sem_grade"]).reshape(-1,1))[0]
+    data["Curricular_units_1st_sem_without_evaluations"] = scaler_Curricular_units_1st_sem_without_evaluations.transform(np.asarray(data["Curricular_units_1st_sem_without_evaluations"]).reshape(-1,1))[0]
+    data["Curricular_units_2nd_sem_credited"] = scaler_Curricular_units_2nd_sem_credited.transform(np.asarray(data["Curricular_units_2nd_sem_credited"]).reshape(-1,1))[0]
+    data["Curricular_units_2nd_sem_enrolled"] = scaler_Curricular_units_2nd_sem_enrolled.transform(np.asarray(data["Curricular_units_2nd_sem_enrolled"]).reshape(-1,1))[0]
+    data["Curricular_units_2nd_sem_evaluations"] = scaler_Curricular_units_2nd_sem_evaluations.transform(np.asarray(data["Curricular_units_2nd_sem_evaluations"]).reshape(-1,1))[0]
+    data["Curricular_units_2nd_sem_approved"] = scaler_Curricular_units_2nd_sem_approved.transform(np.asarray(data["Curricular_units_2nd_sem_approved"]).reshape(-1,1))[0]
+    data["Curricular_units_2nd_sem_grade"] = scaler_Curricular_units_2nd_sem_grade.transform(np.asarray(data["Curricular_units_2nd_sem_grade"]).reshape(-1,1))[0]
+    data["Curricular_units_2nd_sem_without_evaluations"] = scaler_Curricular_units_2nd_sem_without_evaluations.transform(np.asarray(data["Curricular_units_2nd_sem_without_evaluations"]).reshape(-1,1))[0]
+
+    df[["pc1_1", "pc1_2", "pc1_3"]] = pca_1.transform(data[pca_numerical_columns_1])
+
+    # PCA 2
+    data["Previous_qualification_grade"] = scaler_Previous_qualification_grade.transform(np.asarray(data["Previous_qualification_grade"]).reshape(-1,1))[0]
+    data["Admission_grade"] = scaler_Admission_grade.transform(np.asarray(data["Admission_grade"]).reshape(-1,1))[0]
+
+    df[["pc2_1"]] = pca_2.transform(data[pca_numerical_columns_2])
+
+    return df
---
+++ graduation_analysis_app.py
@@ -0,0 +1,158 @@
+import streamlit as st
+import pandas as pd
+import joblib
+from data_preprocessing import data_preprocessing,encoder_Application_mode, encoder_Course, encoder_Debtor,encoder_Displaced,encoder_Educational_special_needs,encoder_Gender,encoder_International,encoder_Marital_status,encoder_Previous_qualification,encoder_Scholarship_holder,encoder_Tuition_fees_up_to_date,encoder_attendance
+from prediction import prediction
+
+col1, col2 = st.columns([1, 5])
+with col1:
+    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png", width=130)
+with col2:
+    st.header('graduation analysis App (Prototype)')
+#####
+
+data = pd.DataFrame()
+  
+col1, col2, col3, col4 = st.columns(4)
+  
+with col1:
+    Application_mode = st.selectbox(label='Application_mode', options=encoder_Application_mode.classes_, index=1)
+    data["Application_mode"] = [Application_mode]
+  
+with col2:
+    Course = st.selectbox(label='Course', options=encoder_Course.classes_, index=1)
+    data["Course"] = [Course]
+  
+with col3:
+    Debtor = st.selectbox(label='Debtor', options=encoder_Debtor.classes_, index=1)
+    data["Debtor"] = [Debtor]
+
+with col4:
+    Displaced = st.selectbox(label='Displaced', options=encoder_Displaced.classes_, index=1)
+    data["Displaced"] = [Displaced]
+#
+col1, col2, col3, col4 = st.columns(4)
+  
+with col1:
+    Educational_special_needs = st.selectbox(label='Educational_special_needs', options=encoder_Educational_special_needs.classes_, index=1)
+    data["Educational_special_needs"] = [Educational_special_needs]
+  
+with col2:
+    Gender = st.selectbox(label='Gender', options=encoder_Gender.classes_, index=1)
+    data["Gender"] = [Gender]
+  
+with col3:
+    International = st.selectbox(label='International', options=encoder_International.classes_, index=1)
+    data["International"] = [International]
+
+with col4:
+    Marital_status = st.selectbox(label='Marital_status', options=encoder_Marital_status.classes_, index=1)
+    data["Marital_status"] = [Marital_status]
+#
+col1, col2, col3, col4 = st.columns(4)
+  
+with col1:
+    Previous_qualification = st.selectbox(label='Previous_qualification', options=encoder_Previous_qualification.classes_, index=1)
+    data["Previous_qualification"] = [Previous_qualification]
+  
+with col2:
+    Scholarship_holder = st.selectbox(label='Scholarship_holder', options=encoder_Scholarship_holder.classes_, index=1)
+    data["Scholarship_holder"] = [Scholarship_holder]
+  
+with col3:
+    Tuition_fees_up_to_date = st.selectbox(label='Tuition_fees_up_to_date', options=encoder_Tuition_fees_up_to_date.classes_, index=1)
+    data["Tuition_fees_up_to_date"] = [Tuition_fees_up_to_date]
+
+with col4:
+    attendance = st.selectbox(label='attendance', options=encoder_attendance.classes_, index=1)
+    data["attendance"] = [attendance]
+
+####################################
+col1, col2 = st.columns(2)
+  
+with col1:
+        
+    Admission_grade = int(st.number_input(label='Admission_grade', value=0))
+    data["Admission_grade"] = [Admission_grade]
+  
+with col2:
+    Age_at_enrollment = int(st.number_input(label='Age_at_enrollment', value=0))
+    data["Age_at_enrollment"] = [Age_at_enrollment]
+   
+# 
+col1, col2, col3 = st.columns(3)
+  
+with col1:
+    Curricular_units_1st_sem_approved = int(st.number_input(label='Curricular_units_1st_sem_approved', value=0))
+    data["Curricular_units_1st_sem_approved"] = [Curricular_units_1st_sem_approved]
+  
+with col2:
+    # st.header("Kolom 1")
+    Curricular_units_1st_sem_credited = int(st.number_input(label='Curricular_units_1st_sem_credited', value=0))
+    data["Curricular_units_1st_sem_credited"] = [Curricular_units_1st_sem_credited]
+  
+with col3:
+    Curricular_units_1st_sem_enrolled = int(st.number_input(label='Curricular_units_1st_sem_enrolled', value=0))
+    data["Curricular_units_1st_sem_enrolled"] = [Curricular_units_1st_sem_enrolled]
+  
+#  
+col1, col2, col3 = st.columns(3)
+  
+with col1:
+    Curricular_units_1st_sem_evaluations = int(st.number_input(label='Curricular_units_1st_sem_evaluations', value=0))
+    data["Curricular_units_1st_sem_evaluations"] = [Curricular_units_1st_sem_evaluations]
+  
+with col2:
+    Curricular_units_1st_sem_grade = int(st.number_input(label='Curricular_units_1st_sem_grade', value=0))
+    data["Curricular_units_1st_sem_grade"] = [Curricular_units_1st_sem_grade]
+  
+with col3:
+    Curricular_units_1st_sem_without_evaluations = int(st.number_input(label='Curricular_units_1st_sem_without_evaluations', value=0))
+    data["Curricular_units_1st_sem_without_evaluations"] = [Curricular_units_1st_sem_without_evaluations]
+  
+#  
+col1, col2, col3 = st.columns(3)
+  
+with col1:
+    Curricular_units_2nd_sem_approved = int(st.number_input(label='Curricular_units_2nd_sem_approvedy', value=0))
+    data["Curricular_units_2nd_sem_approved"] = [Curricular_units_2nd_sem_approved]
+  
+with col2:
+    Curricular_units_2nd_sem_credited = int(st.number_input(label='Curricular_units_2nd_sem_credited', value=0))
+    data["Curricular_units_2nd_sem_credited"] = [Curricular_units_2nd_sem_credited]
+  
+with col3:
+    Curricular_units_2nd_sem_enrolled = int(st.number_input(label='Curricular_units_2nd_sem_enrolled', value=0))
+    data["Curricular_units_2nd_sem_enrolled"] = [Curricular_units_2nd_sem_enrolled]
+
+#  
+col1, col2, col3 = st.columns(3)
+  
+with col1:
+    Curricular_units_2nd_sem_evaluations = int(st.number_input(label='Curricular_units_2nd_sem_evaluations', value=0))
+    data["Curricular_units_2nd_sem_evaluations"] = [Curricular_units_2nd_sem_evaluations]
+  
+with col2:
+    Curricular_units_2nd_sem_grade = int(st.number_input(label='Curricular_units_2nd_sem_grade', value=0))
+    data["Curricular_units_2nd_sem_grade"] = [Curricular_units_2nd_sem_grade]
+  
+with col3:
+    Curricular_units_2nd_sem_without_evaluations = int(st.number_input(label='Curricular_units_2nd_sem_without_evaluations', value=0))
+    data["Curricular_units_2nd_sem_without_evaluations"] = [Curricular_units_2nd_sem_without_evaluations]
+
+#  
+col1 = st.columns(1)
+  
+
+Previous_qualification_grade = int(st.number_input(label='Previous_qualification_grade', value=0))
+data["Previous_qualification_grade"] = [Previous_qualification_grade]
+
+
+with st.expander("View the Raw Data"):
+    st.dataframe(data=data, width=800, height=10)
+
+if st.button('Predict'):
+    new_data = data_preprocessing(data=data)
+    with st.expander("View the Preprocessed Data"):
+        st.dataframe(data=new_data, width=800, height=10)
+    st.write("Credit Scoring: {}".format(prediction(new_data)))
